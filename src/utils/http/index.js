126584c4eb1f69200ee4828428141ac2cc1acff3
satisfied_with <1521217265@qq.com>
2026-04-15 19:50:31 +0800

    去掉代码里面的环境地址，Nginx 代理

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -6,8 +6,7 @@
  */
 
 // 基础配置
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
 const DEFAULT_TIMEOUT = 30000 // 30秒
 
